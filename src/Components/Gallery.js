cd5c9f00e772153c57d8d474f216554a3de38a4a
marco <artofmarco@gmail.com>
2018-01-24 14:50:43 -0800

    rsvp email working space 2

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -81,7 +81,7 @@ export default class Gallery extends React.Component {
 				</p>
 				<p>
 					If you already sent your card but didn't get this message in time, please text or 
-					<b><a target='_blank' rel="noopener noreferrer" href='mailto:conorandmarco@gmail.com'> E-mail us.</a></b>
+					<b><a target='_blank' rel="noopener noreferrer" href='mailto:conorandmarco@gmail.com'> E-mail </a></b>us at ConorAndMarco@gmail.com
 					<br/>
 					<b>Thank you!</b>
 				</p>
